2f7d0597ba8557096683f4b99ee88a485d76376f
TereraiQubed <tapiwa.tererai@qubedlab.com>
2021-08-15 23:22:55 +0200

    login and reg working

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,21 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function jsonError($statusCode = 500, $message = "Unexpected Error")
+    {
+
+        return response()->json([
+            "success" => false,
+            "message" => $message
+        ], $statusCode);
+    }
+
+    public function jsonSuccess($statusCode = 200, $message = "Request Successful")
+    {
+        return response()->json([
+            "success" => true,
+            "message" => $message
+        ], $statusCode);
+    }
 }
